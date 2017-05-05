960afd844241c1bb8edf5502ace1bb7ca843861e
Salessawi Ferede Yitbarek <salssaf@umich.edu>
2017-05-05 17:28:21 -0400

    Cleanning up and commenting

--- mee_decryptor.cpp
+++ mee_decryptor.cpp
@@ -30,66 +30,28 @@ Decryptor::Decryptor(FACache *cache_, MEESystem *dram_):
 //**************************
 
 
-// uint64_t Decryptor::get_data_addr(uint64_t addr){
-    
-
-//     auto type =  get_block_type(addr);
-//     unsigned offset;
-//     switch(type){
-//         case(BLOCK):
-//             //print warning
-//             break;
-//         case(MAC):
-//             offset = ( addr - MAC_REGION_START)*MAC_PER_CL;
-            
-
-//             break;
-//         case(VER):
-
-//             break;
-//         case(L0):
-            
-//             break;
-//         case(L1):
-            
-//             break;
-//         case(L2):
-            
-//             break;
-//     }
-
-
-// }
-
-uint64_t get_address_offset(uint64_t data_addr, uint64_t region_size){
-
-    uint64_t offset_bits = log2(region_size);
-
-    //remove the lower (offset_bit) bits
-    //xxxxxxxxxxx to xxxxxx00000
-    uint64_t masked = (data_addr >> offset_bits) << offset_bits;
-
-    //xxxxxxxxxxx ^ xxxxxx00000 = xxxxx
-    uint64_t offset = masked ^ data_addr;
-
-    return offset;
-
-}
-
 //TODO: repetitive....
 //define function that takes region size and region start address after testing
 
+
+
+//get the address for the MAC associated with a data block
 inline uint64_t Decryptor::get_MAC_address(uint64_t data_addr){
+    
+    ////if the CPU simulator does not implement a proper TLB, it might send the raw virtual address. 
+    //this will be problamatic when computing the address for CTR, MAC etc...
+    //so we mask it first
     data_addr = data_addr & VIRT_ADDR_MASK; 
+
     uint64_t offset = (data_addr - DATA_REGION_START) /MAC_PER_CL; 
-    //get_address_offset(data_addr, MAC_REGION_SIZE);
+
+    //align to a 64B block
     return (MAC_REGION_START + offset) & ~0x3f; 
 }
 
 inline uint64_t Decryptor::get_VER_address(uint64_t data_addr){
     data_addr = data_addr & VIRT_ADDR_MASK; 
     uint64_t offset = (data_addr - DATA_REGION_START) /CTR_PER_CL;
-    //get_address_offset(data_addr, VER_REGION_SIZE);
 
     return (VER_REGION_START + offset) & ~0x3f; 
 }
@@ -97,48 +59,52 @@ inline uint64_t Decryptor::get_VER_address(uint64_t data_addr){
 inline uint64_t Decryptor::get_L0_address(uint64_t data_addr){
     data_addr = data_addr & VIRT_ADDR_MASK; 
     uint64_t offset = (data_addr - DATA_REGION_START)/(CTR_PER_CL*CTR_PER_CL);
-    //get_address_offset(data_addr, L0_REGION_SIZE);
     return (L0_REGION_START + offset) & ~0x3f; ;
 }
 
 inline uint64_t Decryptor::get_L1_address(uint64_t data_addr){
     data_addr = data_addr & VIRT_ADDR_MASK; 
     uint64_t offset = (data_addr - DATA_REGION_START)/std::pow(CTR_PER_CL, 3);
-    //get_address_offset(data_addr, L1_REGION_SIZE);
     return (L1_REGION_START + offset) & ~0x3f; 
 }
 
 inline uint64_t Decryptor::get_L2_address(uint64_t data_addr){
     data_addr = data_addr & VIRT_ADDR_MASK; 
     uint64_t offset = (data_addr - DATA_REGION_START)/std::pow(CTR_PER_CL, 4);
-    //get_address_offset(data_addr, L2_REGION_SIZE);
     return (L2_REGION_START + offset) & ~0x3f; 
 }
 
 
 
+//this gives us the index of the first transaction with 
+//this physical address
 int Decryptor::search_trans_by_addr(uint64_t addr){
 
     for (unsigned i = 0; i < transactions_addr.size(); ++i){
         
         if (transactions_addr[i] == addr){
             return i;
-        }
-    
-        
+        }        
     }
 
     return -1;
 
 }
 
+
+//there might be multiple outstanding reads/writes to the same data block
+//in this case, we need to match incoming meta-data(CTR, MAC ...) with the 
+//right data block request. this function returns the index of the oldest 
+//transaction that is waiting for a meta data
+    
 int Decryptor::search_waiting_trans(uint64_t addr, RequestType_ type){
 
     for (unsigned i = 0; i < transactions_addr.size(); ++i){
         
 
         if (transactions_addr[i] == addr && 
-            !transactions_status[i].test( type )){
+            !transactions_status[i].test( type )){ //check this outstanding request 
+                                                   //is waiting for a specific type of metadata
 
             return i;
         }
@@ -152,6 +118,8 @@ int Decryptor::search_waiting_trans(uint64_t addr, RequestType_ type){
 
 //**************************
 
+
+
 bool Decryptor::send_dram_req(bool is_write, uint64_t addr){
 
     if(dram->can_accept_dram_req(addr)){
@@ -163,6 +131,7 @@ bool Decryptor::send_dram_req(bool is_write, uint64_t addr){
     return false;
 }
 
+
 bool Decryptor::send_cache_req(bool is_write, uint64_t addr){
     
     if(cache->can_accept_input()){
@@ -173,19 +142,23 @@ bool Decryptor::send_cache_req(bool is_write, uint64_t addr){
     return false;
 }
 
-void Decryptor::finish_crypto(){
-    
-    //this function is called when a block and all of it's meta-data is fetched
+
+
+
+//*************************
+//this function is called after a block and all of it's meta-data is fetched
     //at this point, the relevant counter values have also passed through the
-    //AES pipeline. also, data must have already been read from SRAM at this point
+    //AES pipeline. also, top level counter must have already been read from SRAM at this point
     //hence, we just need to:
     //-  do GF-mult(1 cycle)
     // - compare output with MAC(1 cycle)
     // - XOR key stream with ... (can be done in parallel with or at the end of the above operation)
     // - total: 2 cycles
 
+void Decryptor::finish_crypto(){
     
-    //schedule a finished event to happen 2 cycles from now.
+
+
 
     if (crypto_finalize_queue.empty()) return;
     
@@ -195,9 +168,7 @@ void Decryptor::finish_crypto(){
 
     MEE_DEBUG("finalizing_crypto\t0x" << hex <<addr);
 
-
-
-
+    //schedule a finished event to happen 2 cycles from now.
     crypto_finalize_pipeline->add_input(addr);
 
 
@@ -224,12 +195,12 @@ void Decryptor::finish_crypto(){
     transactions_status.erase( transactions_status.begin() + trans_idx );
     transactions_addr.erase( transactions_addr.begin() + trans_idx );
 
-
-
-
 }
 
 
+
+//when a read request returns from memory, we use this function to figure out
+//what type of data it is
 Decryptor::RequestType Decryptor::get_block_type(uint64_t type){
 
     if(type >= VER_REGION_START && type <= (VER_REGION_START + VER_REGION_SIZE)){
@@ -257,20 +228,29 @@ Decryptor::RequestType Decryptor::get_block_type(uint64_t type){
 
 }
 
+
+
+//generate DRAM requests (data, MAC, CTR, ...) for a secure read/write request.
+
+//it takes multiple cycles to send all the requests
 void Decryptor::process_active(){
 
      //TODO: this is sub-optimal as we are not sending cache requests when DRAM 
     //request queue if full (i.e if BLOCK request cannot be sent, counter/MAC cache 
     //cannot be read). this will probably have a small effect on performance.
 
-    //generate DRAM requests for a secure read request if we are
-    //not done requesting everything (MAC, CTR, ...)
 
 
     uint64_t addr;
 
+    //this implements a state machine. 
+    //we begin by request a BLOCK in the first cycle
+    //in the next cycle we request a MAC and so on...
+
     switch(active_request_status){
-        case(BLOCK):
+
+        case(BLOCK): //this is the starting state. 
+
             if ( send_dram_req(false, current_active_request) ){
                 active_request_status = VER;
                 MEE_DEBUG("BLOCK req\t0x" << hex << current_active_request);
@@ -346,79 +326,12 @@ void Decryptor::process_active(){
 }
 
 
-/*void Decryptor::mee_process_write(){
-
-    uint64_t addr;
-    switch(active_request_status){
-        case(MAC):
-            addr = get_MAC_address(current_active_request);
-            if(send_cache_req(false, addr)){
-                active_request_status = L0;
-                
-                outstanding_metadata_reads.insert(
-                    make_pair(addr, current_active_request) );
-
-                MEE_DEBUG("MAC req\t0x" << hex << addr);
-            }
-            break;
-        case(VER):
-                
-            addr = get_VER_address(current_active_request);
-            if(send_cache_req(false, addr)){
-                active_request_status = MAC;
-                
-                outstanding_metadata_reads.insert(
-                    make_pair(addr, current_active_request) );
-
-                MEE_DEBUG("VER req\t0x" << hex << addr);
-            }
-            break;
-        case(L0):
-                
-            addr = get_L0_address(current_active_request);
-            if(send_cache_req(false, addr)){
-                active_request_status = L1;
-                
-                outstanding_metadata_reads.insert(
-                    make_pair(addr, current_active_request) );
-
-                MEE_DEBUG("L0 req\t0x" << hex << addr);
-            }
-            break;
-        case(L1):
-                
-            addr = get_L1_address(current_active_request);
-            if(send_cache_req(false, addr)){
-                active_request_status = L2;
-                
-                outstanding_metadata_reads.insert(
-                    make_pair(addr, current_active_request) );
-
-                MEE_DEBUG("L1 req\t0x" << hex << addr);
-            }
-            break;
-        case(L2):
-                
-            addr = get_L2_address(current_active_request);
-            if(send_cache_req(false, addr)){
-                //we are done requesting everything
-                request_is_active = false;
-                
-                outstanding_metadata_reads.insert(
-                    make_pair(addr, current_active_request) );
-
-                MEE_DEBUG("L2 req\t0x" << addr);
-            }
-            break;
-    }
-
-}*/
-
+//this is the function that takes data off of a request queue
 void Decryptor::process_mee_input(){
     
 
     //a memory request is processed as follows (cycle numbers ignore possible stalls):
-    //cycle 0: allocate space in table for outstanding requests (a CAM searched by address or tag)
+    //cycle 0: allocate space in table for outstanding requests (a CAM searched by address)
     //cycle 0: request the memory block(512 bits)
     //cycle 1: request block with version number (512 bits) - remeber performing integrity checking requires the full mem block
     //cycle 2: request MAC for data (56 bits) 
@@ -434,14 +347,18 @@ void Decryptor::process_mee_input(){
 
     
 
-    if(request_is_active){
+    //if we are not done sending requests for MAC, CTR, etc ....
+    //we proceed to do that
 
+    if(request_is_active){
 
         process_active();
 
     }
 
 
+    //we can process a new request once we are done generating MAC, CTR ... requests
+    //for a previous requests
     bool process_new_request = !input_queue.empty() & !request_is_active;
     if(process_new_request){
 
@@ -470,9 +387,6 @@ void Decryptor::process_mee_input(){
             active_request_status = BLOCK;    
         }
     
-
-        //@@transactions[current_active_request] = status;
-
         transactions_status.push_back(status);
         //current_trans_index = transactions_status.size()-1;
 
@@ -489,43 +403,44 @@ void Decryptor::process_mee_input(){
 }
 
 
+//*********************************************
+//this function is used to update the status of outstanding requests
+//it is called when a certain data is ready
+//i.e. data arrives from memory, CTR has passed through AES pipeline
 void Decryptor::update_status(uint64_t addr){
+
+    //FIXME: is 1 cycle too optimistic for this?
+
 
     auto type = get_block_type(addr);
     auto entry = outstanding_metadata_reads.find(addr);
 
     uint64_t data_addr; 
-    if(type==BLOCK){
+
+    if(type == BLOCK){
         data_addr = addr;
     }
     else {
+
+        //erase from our outstanding metadata request list
         data_addr = entry->second;    
         outstanding_metadata_reads.erase( entry );
     }
     
-    //@@bool sram_hit = transactions.count(data_addr) > 0;
 
+
+    //
     int trans_idx = search_waiting_trans(data_addr, type);
-
     bool sram_hit = trans_idx  != -1;
 
     if(sram_hit){
         
         MEE_DEBUG(RequestTypeStr[type]  << "_READY\t0x" << hex << addr << "\t0x" << hex << data_addr );
 
-        //@@transactions[data_addr].set(type);
         transactions_status[trans_idx].set(type);
         
         //check if that request has all what it needs and 
         //start decryption/re-encryption
-
-        //@@ bool ready = transactions[data_addr].test(L2) &&
-        //              transactions[data_addr].test(L1) &&
-        //              transactions[data_addr].test(L0) &&
-        //              transactions[data_addr].test(MAC) &&
-        //              transactions[data_addr].test(VER)  &&
-        //              transactions[data_addr].test(BLOCK);
-
         bool ready = transactions_status[trans_idx].test(L2) &&
                      transactions_status[trans_idx].test(L1) &&
                      transactions_status[trans_idx].test(L0) &&
@@ -548,13 +463,17 @@ void Decryptor::update_status(uint64_t addr){
 }
 
 
-//read queue the cache and DRAM responses
+//read and queue the cache and DRAM responses
 void Decryptor::read_response(){
+
+
 
     if( dram->is_dram_resp_available() ){
         uint64_t addr = dram->get_dram_response();
 
-        //avoid snooping responses for the ctr/mac cache
+        //to avoid snooping responses for the ctr/mac cache
+        //we make sure the address corresponds to something we requested.
+        //in an actual hardware, there probably won't be such bus sharing
         auto search = std::find(outstanding_dram.begin(), outstanding_dram.end(),
                   addr);
 
@@ -574,10 +493,13 @@ void Decryptor::read_response(){
 
 }
 
+
+//******************
+//processes responses returned from DRAM and meta-data caches
 void Decryptor::process_response(){
     
     //when a DRAM response arrives
-    //1. determine the type of response (cycle 1)
+    //1. determine the type of response - i.e. data block, MAC ... (cycle 1)
     //2. use tag of request and type of request to index and write into the right SRAM array (cycle 2)
     //3. feed the right input into the AES queue (cycle 2, 3, 4, 5) - latency already captured 
     //by queuing delay inside aes_input_queue
@@ -591,19 +513,17 @@ void Decryptor::process_response(){
     response_queue.pop();
 
     //MEE_DEBUG("DRAM response\t0x"<< hex << addr);
-    
-    //match request with an outstanding request and modify bit in SRAM
-    //uint64_t data_addr = get_data_addr(addr);
 
 
+    //1. determine the type of response - i.e. data block, MAC
     auto type = get_block_type(addr);
 
-    //these are counter values and need to pass through 
-    //an AES pipeline to compute a keysteeam or a MAC
-    //(the MAC is )
+    //these are counter values and need to pass through  an AES pipeline.
+    //We will later use the output of the AES pipeline to encrypt/decrypt and compute MAC
+    //(note that we assume the MAC is XOR'd with the keystream)
     if( type == VER ) {
 
-        //we need to decrypt 64 bytes, so we need 4 AES blocks
+        //we need to encrypt/decrypt 64 bytes, so we need 4 AES blocks
         
         //HACK: feeding 0's into the pipline is like introducing a bubble.
         //having 3 bubbles before feeding the actual address will effectively give us
@@ -617,16 +537,16 @@ void Decryptor::process_response(){
 
     }
     
-    //counters from L0, L1, L2 are used to mask the MAC, so we only need 1 block
+    //counters from L0, L1, L2 are used to mask the MAC (not for encryption/decryption)
+    //so we only need 1 block
     else if (type == L0 || type == L1 || type == L2  ){
 
         MEE_DEBUG("AES Start\t0x" << hex << addr);
         aes_input_queue.push(addr);
     }
 
-    //other values just need to be stored
-    //NOTE: in an RTL implementation, we would still need to store some
-    //data from fetched blocks on top of passing it through the AES pipeline
+    //other values just need to be stored temporarily
+    
     else if(type == BLOCK || type == MAC){
         update_status(addr);
     }
@@ -642,6 +562,8 @@ void Decryptor::process_response(){
 }
 
 
+//we use a dummy pipeline at the end to add the extra latenciec that are required
+//for reading scratchpad comparing MAC, XOR'ing key streams etc ...
 void Decryptor::process_final_pipeline(){
 
     //inputs to the pipeline are added by the finalize_crypto function.
@@ -666,6 +588,8 @@ void Decryptor::process_final_pipeline(){
 
 }
 
+//this reads outputs from the AES pipeline and
+//update the state of a request
 void Decryptor::process_aes_pipeline(){
     
     
@@ -692,27 +616,27 @@ void Decryptor::process_aes_pipeline(){
 
 
 
-
-
+//this function has to be called every cycle
 void Decryptor::tick(){
 
 
+    current_cycle++;
+
     process_response(); //process responses that was enqueued the prev cycles
 
-    //read new response. 
-    //calling this second helps introduce a 1 cycle latency before any response is processed
+    //calling this after process response helps introduce a 1 cycle latency before any response is processed
     read_response(); 
 
     process_mee_input();  
     process_aes_pipeline();
     process_final_pipeline();
     finish_crypto();
-    current_cycle++;
     
-    
-
 } 
 
+
+
+//the core/caches uses this function to send memory requests
 bool Decryptor::add_input(bool is_write, uint64_t address){
     
 //    assert(can_accept_input() && "Decryptor: cannot take input this cycle" );
@@ -737,12 +661,15 @@ bool Decryptor::can_accept_input(){
 
 }
 
+
 bool Decryptor::is_output_ready(){
 
     return !output_queue.empty();
 
 }
 
+//the cores/caches use this function to know which memory 
+//returns the address of the request that has completed
 uint64_t Decryptor::get_output(){
 
 //    assert(is_output_ready() && "No output available");
